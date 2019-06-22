cd920f2f09b9453020c0c25744b0809ec86d8da3
kirtibajaj <kirti.bajaj93@gmail.com>
2019-06-22 23:38:41 +0530

    22 JUNE 2019

---
+++ day21/decodeinword.py
@@ -0,0 +1,8 @@
+import urllib.request, urllib.error, urllib.parse
+fhand=urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+counts=dict()
+for line in fhand:
+    words=line.decode().split()
+    for word in words:
+        counts[word]=counts.get(word,0)+1
+print(counts)
